7e2649e73e7edb6d4a1d6ea46bad3ef2ee64a7d9
Skrpion <nestor.martinez.c@uniautonoma.edu.co>
2023-10-28 00:34:13 -0500

    test: nuevos test

--- src/app/services/book.service.spec.ts
+++ src/app/services/book.service.spec.ts
@@ -3,6 +3,7 @@ import { BookService } from "./book.service";
 import { TestBed } from "@angular/core/testing";
 import { Book } from "../models/book.model";
 import { environment } from "src/environments/environment";
+import Swal from "sweetalert2";
 
 const listDummyBooks: Book[] = [
   {
@@ -25,6 +26,17 @@ const listDummyBooks: Book[] = [
   },
 ];
 
+const bookToAdd: Book = {
+  name: 'Book A',
+  author: 'Author A',
+  isbn: 'ISBN A',
+  description: 'Description A',
+  photoUrl: 'Photo A',
+  price: 10,
+  amount: 1, // Ensure that amount is set to 1
+};
+
+
 describe('BookService', () => {
 
 
@@ -45,10 +57,19 @@ describe('BookService', () => {
     _bookService = TestBed.inject(BookService);
     httpMock = TestBed.inject(HttpTestingController);
 
+    // Resetear el storage siempre
+    storage = {};
+
     // Simula el comportamiento de localStorage.getItem para controlar lo que devuelve el método durante las pruebas.
     spyOn(localStorage, 'getItem').and.callFake((key: string) => {
       return storage[key] ? JSON.stringify(storage[key]) : null;
     });
+
+    // Simula el comportamiento de localStorage.setItem
+    spyOn(localStorage, 'setItem').and.callFake((key: string, value: string) => {
+      return storage[key] = value;
+    });
+
   });
 
   // Evitar que no haya peticiones pendientes entre cada test
@@ -81,13 +102,65 @@ describe('BookService', () => {
     expect(books).toEqual([]);
   });
 
-   it('public getBooksFromCart(): Book[], should return books from the cart', () => {
+  it('public getBooksFromCart(): Book[], should return books from the cart', () => {
     // Simula libros en el carrito (localStorage con datos válidos)
     storage['listCartBook'] = listDummyBooks;
 
     const books = _bookService.getBooksFromCart();
 
     expect(books).toEqual(listDummyBooks);
+  });
+
+  it('public addBookToCart(): void, should add a book to an empty cart', () => {
+
+    const toast = {
+      fire: () => null
+    } as any;
+
+    const spy1 = spyOn(Swal, 'mixin').and.callFake( () => {
+      return toast;
+    });
+
+    let listBooks = _bookService.getBooksFromCart();
+    expect(listBooks.length).toBe(0);
+
+    _bookService.addBookToCart(bookToAdd);
+
+    listBooks = _bookService.getBooksFromCart();
+
+    expect(spy1).toHaveBeenCalled();
+  });
+
+  it('public removeBooksFromCart(), should clear the cart in localStorage', () => {
+    // Simula que hay libros en el carrito
+    storage['listCartBook'] = [
+      {
+        id: 1,
+        name: 'Book A',
+        author: 'Author A',
+        isbn: 'ISBN A',
+        description: 'Description A',
+        photoUrl: 'Photo A',
+        price: 10,
+        amount: 1,
+      },
+      {
+        id: 2,
+        name: 'Book B',
+        author: 'Author B',
+        isbn: 'ISBN B',
+        description: 'Description B',
+        photoUrl: 'Photo B',
+        price: 20,
+        amount: 2,
+      },
+    ];
+
+    _bookService.removeBooksFromCart();
+
+    const cart = storage['listCartBook'];
+
+    expect(cart).toBeNull(); // Se espera que 'listCartBook' esté configurado como null
   });
 
 })
